455dadf6d9f53406ff1914539ddc82051a5a112e
yusufakdenizxd <yusufakdnz910@gmail.com>
2023-09-19 12:42:00 +0300

    neotree hidden files visible

--- nvim/lua/extensions/neotree.lua
+++ nvim/lua/extensions/neotree.lua
@@ -16,6 +16,11 @@ neotree.setup({
   },
   filesystem = {
     follow_current_file = true,
+     filtered_items = {
+      visible = true,
+      hide_dotfiles = false,
+      hide_gitignored = true,
+    },
   },
 })
 
