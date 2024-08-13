2cb3fab3dfa265df814b65405993eb60fbf098b8
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2024-08-13 15:39:28 +0300

    feat(tmux-resurrect-fzf): fzf layout reversed

--- zsh/script/tmux-resurrect-fzf.sh
+++ zsh/script/tmux-resurrect-fzf.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 SELECTED_PROJECTS=$(tmuxinator list -n |
     tail -n +2 |
-    fzf --tmux center,20%,30% --prompt="Sessions: " -m -1 -q "$1")
+    fzf --tmux center,20%,30% --layout reverse --prompt="Sessions: " -m -1 -q "$1")
 
 if [ -n "$SELECTED_PROJECTS" ]; then
     IFS=$'\n'
