0cb4a1aee742141221b6c5582ddfd144293eb7f9
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2026-01-05 23:21:00 +0100

    nvim remove ai shit

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -2,7 +2,6 @@ return {
 	"saghen/blink.cmp",
 	dependencies = {
 		{ "rafamadriz/friendly-snippets" },
-		{ "giuxtaposition/blink-cmp-copilot" },
 	},
 	version = "v0.*",
 	opts = {
@@ -28,23 +27,8 @@ return {
 					module = "render-markdown.integ.blink",
 					fallbacks = { "lsp" },
 				},
-				copilot = {
-					name = "copilot",
-					module = "blink-cmp-copilot",
-					score_offset = 100,
-					async = true,
-					transform_items = function(_, items)
-						local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-						local kind_idx = #CompletionItemKind + 1
-						CompletionItemKind[kind_idx] = "Copilot"
-						for _, item in ipairs(items) do
-							item.kind = kind_idx
-						end
-						return items
-					end,
-				},
 			},
-			default = { "snippets", "lsp", "path", "buffer", "copilot", "markdown" },
+			default = { "snippets", "lsp", "path", "buffer", "markdown" },
 		},
 
 		appearance = {
